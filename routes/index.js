a90a0c1e4560e945e0cb8709ee760aa1aff4eac1
fiddle-c <kflor@mailfence.com>
2025-11-09 23:20:21 -0800

    added styles

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,15 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
-router.get('/', async (req, res) => { 
-    let books
-    try {
-        console.log('calling books')
-        books = await Book.find().sort({createdAt: 'desc'}).limit(10).exec()
 
-
-    } catch {
-        console.log('failed to get books')
-        books = []
-    }
-    res.render('index', { books: books })    
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
 })
 
 module.exports = router
